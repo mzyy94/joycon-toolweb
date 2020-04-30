e00019d023670a2f654d2d9c8043f7bb0b627c8e
Yuki MIZUNO <mizuyuu0904@gmail.com>
2020-04-30 20:33:25 +0900

    Connect device

--- index.js
+++ index.js
@@ -11,12 +11,26 @@ const setupJoyconStyle = (object, style) => {
   style.sheet.addRule(".left-joycon-body", "fill: #fff !important");
 };
 
+const connectController = () =>
+  navigator.hid
+    .requestDevice({ filters: [{ vendorId: 0x057e }] })
+    .then(devices => {
+      devices.forEach(async device => {
+        if (!device.opened) {
+          await device.open();
+        }
+        console.log(device.productName, "connected");
+      });
+    });
+
 const main = () => {
   const object = document.querySelector("object");
   const style = document.createElement("style");
   object.addEventListener("load", ({ target: object }) => {
     setupJoyconStyle(object, style);
   });
+  const button = document.querySelector("button");
+  button.addEventListener("click", connectController);
 };
 
 document.addEventListener("DOMContentLoaded", main);
